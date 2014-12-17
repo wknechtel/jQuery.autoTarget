6dd95f03c38886b7224e5e2512455b477d0795fa
Bill Knechtel <billknechtel@me.com>
2014-12-16 19:43:48 -0600

    Remove unnecessary trailing slash in the regex

--- jQuery.autoTarget.js
+++ jQuery.autoTarget.js
@@ -1,6 +1,6 @@
 /*
   jQuery autoTarget Plugin v1.0.0
-  Copyright (c) 2013 William Knechtel
+  Copyright (c) 2013-2014 William Knechtel
   Released under the terms of the MIT Open Source License
     http://opensource.org/licenses/MIT
     
@@ -9,7 +9,7 @@
 
 (function( $ ) {
   $(document).ready(function() {
-    var autoTargetUrlPieces = window.location.href.match(/(http[s]?:\/\/[a-zA-Z0-9.]*)\/(.*)/);
+    var autoTargetUrlPieces = window.location.href.match(/(http[s]?:\/\/[a-zA-Z0-9.]*)(.*)/);
     $("a[href^='http']").not("[href^='" + autoTargetUrlPieces[1] + "']").attr("target", "_blank").addClass("external-link");
   });
 })( jQuery );
